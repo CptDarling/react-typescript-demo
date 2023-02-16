d14af04ca122f63958e8f636af0653df901c5784
Richard Holyoak <rholyoak@gmail.com>
2023-02-16 16:41:14 +0000

    Initial App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, BrowserRouter as Router, Routes, Outlet } from "react-router-dom";
 
-function App() {
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Product({ match }) {
+  console.log(match);
+  return <h2>This is a page for product with ID: {match.params.id}</h2>;
+}
+
+function Layout() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products/1">First Product</Link>
+          </li>
+          <li>
+            <Link to="/products/2">Second Product</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* This is where the child route elements will be rendered */}
+      <Outlet />
     </div>
   );
 }
 
-export default App;
+function AppRouter() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Index />} />
+          <Route path="/products/:id" element={<Product />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+
+}
+
+export default AppRouter;
